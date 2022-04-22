ad674cc08556e53dd77561c49ac7190e1f49010e
Julio Ramirez <jramirez@ectotec.com>
2022-04-21 20:21:31 -0500

    implement the model Person.cs

---
+++ CursoNETCompleto/Models/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoNETCompleto.Models
+{
+    public class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public DateTime DateCreate { get; set; }
+
+        public Person() { }
+
+        public Person(long id, string name, string mail, DateTime dateCreate)
+        {
+            Id = id;
+            Name = name;
+            Mail = mail;
+            DateCreate = dateCreate;
+        }
+
+        
+
+    }
+
+
+
+}
