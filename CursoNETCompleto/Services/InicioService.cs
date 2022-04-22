398fdad048df0127cbbcb24490df7780e6454839
Julio Ramirez <jramirez@ectotec.com>
2022-04-21 20:11:37 -0500

    rename the variable lector -> oracleDataReader

--- CursoNETCompleto/Services/InicioService.cs
+++ CursoNETCompleto/Services/InicioService.cs
@@ -27,7 +27,7 @@ namespace CursoNETCompleto.Services
                         oracleCommand.CommandType = System.Data.CommandType.StoredProcedure;
                         oracleCommand.BindByName = true;
 
-                        oracleCommand.Parameters.Add(new OracleParameter("salida", OracleDbType.RefCursor)
+                        oracleCommand.Parameters.Add(new OracleParameter("OUT_INFO", OracleDbType.RefCursor)
                         {
                             Direction = ParameterDirection.ReturnValue
                         });
@@ -37,29 +37,29 @@ namespace CursoNETCompleto.Services
                         try
                         {
                             // Revisamos si se pudo ejecutar la consulta
-                            OracleDataReader lector = ((OracleRefCursor)oracleCommand.Parameters["salida"].Value).GetDataReader();
+                            OracleDataReader oracleDataReader = ((OracleRefCursor)oracleCommand.Parameters["salida"].Value).GetDataReader();
 
                             // Revisamos cada contacto
-                            while (lector.Read())
+                            while (oracleDataReader.Read())
                             {
                                 try
                                 {
                                     /*Bloqueos bloqueo = new Bloqueos
                                     {
                                         Pidm = Pidm,
-                                        CODIGO = lector["CODIGO"]?.ToString(),
-                                        MOTIVO = lector["MOTIVO"]?.ToString(),
-                                        MONTO = lector["MONTO"]?.ToString(),
-                                        SPRHOLD_USER = lector["SPRHOLD_USER"]?.ToString(),
-                                        BLOQUEA_INSCRIPCION = lector["BLOQUEA_INSCRIPCION"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_PAGO = lector["BLOQUEA_PAGO"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_TRANSCRIPT = lector["BLOQUEA_TRANSCRIPT"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_GRADUACION = lector["BLOQUEA_GRADUACION"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_CALIF = lector["BLOQUEA_CALIF"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_ENROLL = lector["BLOQUEA_ENROLL"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_APPL = lector["BLOQUEA_APPL"]?.ToString() == "N" ? false : true,
-                                        BLOQUEA_EVALUA = lector["BLOQUEA_EVALUA"]?.ToString() == "N" ? false : true,
-                                        SPRHOLD_DATA_ORIGIN = lector["SPRHOLD_DATA_ORIGIN"]?.ToString()
+                                        CODIGO = oracleDataReader["CODIGO"]?.ToString(),
+                                        MOTIVO = oracleDataReader["MOTIVO"]?.ToString(),
+                                        MONTO = oracleDataReader["MONTO"]?.ToString(),
+                                        SPRHOLD_USER = oracleDataReader["SPRHOLD_USER"]?.ToString(),
+                                        BLOQUEA_INSCRIPCION = oracleDataReader["BLOQUEA_INSCRIPCION"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_PAGO = oracleDataReader["BLOQUEA_PAGO"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_TRANSCRIPT = oracleDataReader["BLOQUEA_TRANSCRIPT"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_GRADUACION = oracleDataReader["BLOQUEA_GRADUACION"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_CALIF = oracleDataReader["BLOQUEA_CALIF"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_ENROLL = oracleDataReader["BLOQUEA_ENROLL"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_APPL = oracleDataReader["BLOQUEA_APPL"]?.ToString() == "N" ? false : true,
+                                        BLOQUEA_EVALUA = oracleDataReader["BLOQUEA_EVALUA"]?.ToString() == "N" ? false : true,
+                                        SPRHOLD_DATA_ORIGIN = oracleDataReader["SPRHOLD_DATA_ORIGIN"]?.ToString()
                                     };
                                     bloqueos.Add(bloqueo);*/
                                 }
